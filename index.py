78bb0d05626cd5e5c6f8a141a428cf78ae660756
Juliette Lonij <jlonij@gmail.com>
2017-02-21 22:18:47 +0100

    Clean up index.py

--- index.py
+++ index.py
@@ -5,21 +5,20 @@ import json
 import record
 import requests
 
-SOLR = "http://sara-backup:8082/solr/dbpedia/"
-SOLR_ADD = SOLR + 'update/json/docs'
+SOLR_URL = 'http://sara-backup:8082/solr/dbpedia/update/json/docs'
 
 headers = {'Content-Type': 'application/json'}
 
-with open('uris_nl.txt', 'r') as fh:
+with open('uris_nl.txt', 'rb') as fh:
     for line in fh:
         try:
-            r = record.index(line[:-1].decode('utf-8'))
-            payload = json.dumps(r, ensure_ascii=False).encode('utf-8')
+            rec = record.index(line[:-1].decode('utf-8'))
+            payload = json.dumps(rec, ensure_ascii=False).encode('utf-8')
             print(payload)
-            r = requests.post(SOLR_ADD, data=payload, headers=headers)
+            r = requests.post(SOLR_URL, data=payload, headers=headers)
             print(r.text)
         except:
-            with open('uris_nl_skipped.txt', 'a') as fh:
+            with open('uris_nl_skipped.txt', 'ab') as fh:
                 fh.write(line)
             continue
 
